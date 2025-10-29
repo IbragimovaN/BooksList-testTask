f0499a6d026a59999a7ed784db0147b1edc217c8
Anastasiya <anastasiya.ibr@mail.ru>
2025-10-29 12:09:29 +0300

    frat: created pagr layout component

---
+++ src/components/common/PageLayout/PageLayout.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+import styles from "./Page.layout.module.css";
+import { Spinner } from "../Spinner/Spinner";
+
+interface IProps {
+  children: ReactNode;
+  isLoading: boolean;
+  error: string | null;
+}
+export const PageLayout = ({ children, isLoading, error }: IProps) => {
+  return (
+    <div className={styles.page}>
+      {isLoading ? (
+        <Spinner />
+      ) : error ? (
+        <div className={styles.error}>{error}</div>
+      ) : (
+        <>{children}</>
+      )}
+    </div>
+  );
+};
