c0f2c764de94feac6dd72b703b7705a838848960
Anastasiya <anastasiya.ibr@mail.ru>
2025-11-01 11:51:09 +0300

    fix: fix literal error

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    {" "}
     <BrowserRouter>
       <App />
     </BrowserRouter>
